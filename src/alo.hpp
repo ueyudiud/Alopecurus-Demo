68bd095af4c8175957e2cae88ec8f23d1d904805
ueyudiud <ueyudiud@163.com>
2019-10-04 12:54:57 +0800

    Add API in C++
    [Added]alo.hpp file, which is Alopecurus API in C++.

---
+++ src/alo.hpp
@@ -0,0 +1,21 @@
+/*
+ * alo.hpp
+ *
+ * Alopecurus API for C++
+ *
+ *  Created on: 2019年10月2日
+ *      Author: ueyudiud
+ */
+
+#ifndef ALO_HPP_
+#define ALO_HPP_
+
+extern "C" {
+
+#include "alo.h"
+#include "aaux.h"
+#include "alibs.h"
+
+}
+
+#endif /* ALO_HPP_ */
