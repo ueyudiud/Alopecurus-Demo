f3fce169a7b0a140621c22aabead28a4a8295490
ueyudiud <ueyudiud@163.com>
2019-10-07 14:28:43 +0800

    Add run function in system library.
    [Added]'run' function in system library, used to run command in shell.

--- src/asyslib.c
+++ src/asyslib.c
@@ -12,7 +12,10 @@
 #include "alibs.h"
 
 #include <stdlib.h>
+#include <errno.h>
 #include <time.h>
+
+#define alo_pusherrcode alo_pushinteger
 
 static int sys_exit(astate T) {
 	int status = aloL_getoptinteger(T, 0, 0);
@@ -33,10 +36,39 @@ static int sys_clock(astate T) {
 	return 1;
 }
 
+/**
+ ** run command in shell, if no parameter exist, return true if
+ ** shell is available. If parameter is present, try to run command.
+ ** The function has two results, first is a boolean value, when
+ ** run meet an error, it return false.
+ ** prototype: sys.run([cmd])
+ */
+static int sys_run(astate T) {
+	astr cmd = aloL_getoptstring(T, 0, NULL);
+	int stat = system(cmd);
+	if (cmd) {
+		if (stat == -1) { /* error? */
+			int no = errno;
+			alo_pushboolean(T, false);
+			alo_pusherrcode(T, no);
+		}
+		else {
+			alo_pushboolean(T, true);
+			alo_pushinteger(T, stat);
+		}
+		return 2;
+	}
+	else {
+		alo_pushboolean(T, stat);
+		return 1;
+	}
+}
+
 static const acreg_t mod_funcs[] = {
 	{ "exit", sys_exit },
-	{ "getenv", sys_getenv },
 	{ "clock", sys_clock },
+	{ "getenv", sys_getenv },
+	{ "run", sys_run },
 	{ NULL, NULL }
 };
 
